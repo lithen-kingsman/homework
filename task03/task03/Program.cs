5a6ff5daf0fd89a6c20ed6ee18e49da93c7e3887
lithen <luzon.t.06@gmail.com>
2022-10-13 23:01:46 +0300

    Домашняя работа номер 3. Целочисленная арифметика

---
+++ task03/task03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое трехзначное число n, в котором число десятков не равно нулю: ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            var number1 = n / 100;
+            var number2 = n % 10;
+            var number3 = (n/10) % 10;
+
+            int x = number1 * 100 + number2 * 10 + number3; 
+
+            Console.WriteLine("Первоначальное число x равно: " + x);
+
+            Console.ReadKey();
+        }
+    }
+}
