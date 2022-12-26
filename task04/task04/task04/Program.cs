cfe0582c1172d89860a7e3cec831c74832f87e97
lithen <luzon.t.06@gmail.com>
2022-12-27 01:35:00 +0500

    задание 4

---
+++ task04/task04/task04/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace task04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число x:");
+
+            double x = 0;
+
+            try
+            {
+               x = double.Parse(Console.ReadLine());
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
+            var firstsummand = x * x;
+            var secondsummand = Math.Sqrt(x*x + 1/(x*x));
+            var summ = firstsummand + secondsummand;
+
+            var y = Math.Sqrt(summ);
+
+            Console.WriteLine("Значение значение функции y = f(x) равно:" + y);
+
+            Console.ReadKey();
+
+        }
+    }
+}
