55308df572ea6391365bd20c406fbf9c331371fe
lithen <luzon.t.06@gmail.com>
2022-12-26 16:59:54 +0300

    задание 3

---
+++ task04/task04/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace task04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число x:");
+
+            double x = 0;
+
+            try
+            {
+               x = double.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            var y = Math.Sqrt(x * x + Math.Sqrt(x * x + 1 / (x * x)));
+
+            Console.WriteLine("Значение значение функции y = f(x) равно:" + y);
+
+            Console.ReadKey();
+
+        }
+    }
+}
